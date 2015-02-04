591167941b6ca816e30f1d56dafea063829870f6
Fredrik Lindström <frli01@gmail.com>
2015-02-04 15:32:02 +0100

    Generated java classes and added deserialisation example

--- ticket/sample/ticket/JavaTicketSubscriber/src/TicketSubscriber.java
+++ ticket/sample/ticket/JavaTicketSubscriber/src/TicketSubscriber.java
@@ -1,7 +1,8 @@
+import javax.xml.bind.JAXB;
 import com.microsoft.windowsazure.Configuration;
 import com.microsoft.windowsazure.services.servicebus.*;
 import com.microsoft.windowsazure.services.servicebus.models.*; 
-
+import se.qbranch.tickets.Ticket;
 
 public class TicketSubscriber {
 	public static void main(String[] args) {
@@ -14,44 +15,38 @@ public class TicketSubscriber {
 			      ".servicebus.windows.net",
 			      "-sb.accesscontrol.windows.net/WRAPv0.9");
 
-			ServiceBusContract service = ServiceBusService.create(config);
+		ServiceBusContract service = ServiceBusService.create(config);
 
-
-			try
-			{
-			    ReceiveMessageOptions opts = ReceiveMessageOptions.DEFAULT;
-			    opts.setReceiveMode(ReceiveMode.PEEK_LOCK);
-
-			    while(true)  { 
-			        ReceiveSubscriptionMessageResult  resultSubMsg = service.receiveSubscriptionMessage("message-out-from-qnet", "2822-ticket", opts);
-			        BrokeredMessage message = resultSubMsg.getValue();
-			        if (message != null && message.getMessageId() != null)
-			        {
-			        	System.out.println(message.getMessageId());
-			        	System.out.println(message.getProperty("customerid"));
-			        	System.out.println(message.getProperty("messagetype"));
-			        	
-//			        	byte[] b = new byte[200];
-//			            String s = null;
-//			            int numRead = message.getBody().read(b);
-//			            while (-1 != numRead)
-//			            {
-//			                s = new String(b);
-//			                s = s.trim();
-//			                System.out.print(s);
-//			                numRead = message.getBody().read(b);
-//			            }
-//			            System.out.println();
-			        	
-			        }
-			        }
-			}
-			catch(Exception e)
-			{
-				System.out.print("ServiceException encountered: ");
-			    System.out.println(e.getMessage());
-			    System.exit(-1);
-			}
+		try
+		{
+		    ReceiveMessageOptions opts = ReceiveMessageOptions.DEFAULT;
+		    opts.setReceiveMode(ReceiveMode.PEEK_LOCK);
+		 
+		    while(true)  { 
+		        ReceiveSubscriptionMessageResult  resultSubMsg = service.receiveSubscriptionMessage("message-out-from-qnet-dev", "8001-ticket", opts);
+		        BrokeredMessage message = resultSubMsg.getValue();
+		        
+		        if (message != null && message.getMessageId() != null)
+		        {	
+		        	Ticket t = JAXB.unmarshal(message.getBody(), Ticket.class);
+		        	
+		        	System.out.println(t.getTicketId());
+		        	System.out.println(t.getCustomerId());
+		        	System.out.println(t.getTicketDescription());
+		        	System.out.println("-----------");
+		        }
+		        else
+		        {
+		        	System.out.println("No new messages");
+		        }
+	        }
+		}
+		catch(Exception e)
+		{
+			System.out.print("ServiceException encountered: ");
+		    System.out.println(e.getMessage());
+		    System.exit(-1);
+		}
 	}
 }
 
