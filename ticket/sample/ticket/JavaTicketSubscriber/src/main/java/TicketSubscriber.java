9882cd8ebb3d9cee8bf8c4cac7a09351a22e3a58
Mårten Gustafson <marten.gustafson@gmail.com>
2015-02-06 19:12:47 +0100

    Tabs, meet spaces + blank line annihilation.

--- ticket/sample/ticket/JavaTicketSubscriber/src/main/java/TicketSubscriber.java
+++ ticket/sample/ticket/JavaTicketSubscriber/src/main/java/TicketSubscriber.java
@@ -1,52 +1,50 @@
-import javax.xml.bind.JAXB;
-import com.microsoft.windowsazure.Configuration;
-import com.microsoft.windowsazure.services.servicebus.*;
-import com.microsoft.windowsazure.services.servicebus.models.*; 
 import se.qbranch.tickets.Ticket;
 
+import javax.xml.bind.JAXB;
+
+import com.microsoft.windowsazure.Configuration;
+import com.microsoft.windowsazure.services.servicebus.ServiceBusConfiguration;
+import com.microsoft.windowsazure.services.servicebus.ServiceBusContract;
+import com.microsoft.windowsazure.services.servicebus.ServiceBusService;
+import com.microsoft.windowsazure.services.servicebus.models.BrokeredMessage;
+import com.microsoft.windowsazure.services.servicebus.models.ReceiveMessageOptions;
+import com.microsoft.windowsazure.services.servicebus.models.ReceiveMode;
+import com.microsoft.windowsazure.services.servicebus.models.ReceiveSubscriptionMessageResult;
+
 public class TicketSubscriber {
-	public static void main(String[] args) {
-		
-		Configuration config = 
-			    ServiceBusConfiguration.configureWithWrapAuthentication(
-			      "namespace",
-			      "user",
-			      "password",
-			      ".servicebus.windows.net",
-			      "-sb.accesscontrol.windows.net/WRAPv0.9");
 
-		ServiceBusContract service = ServiceBusService.create(config);
-
-		try
-		{
-		    ReceiveMessageOptions opts = ReceiveMessageOptions.DEFAULT;
-		    opts.setReceiveMode(ReceiveMode.PEEK_LOCK);
-		 
-		    while(true)  { 
-		        ReceiveSubscriptionMessageResult  resultSubMsg = service.receiveSubscriptionMessage("topic", "subscription", opts);
-		        BrokeredMessage message = resultSubMsg.getValue();
-		        
-		        if (message != null && message.getMessageId() != null)
-		        {	
-		        	Ticket t = JAXB.unmarshal(message.getBody(), Ticket.class);
-		        	
-		        	System.out.println(t.getTicketId());
-		        	System.out.println(t.getCustomerId());
-		        	System.out.println(t.getTicketDescription());
-		        	System.out.println("-----------");
-		        }
-		        else
-		        {
-		        	System.out.println("No new messages");
-		        }
-	        }
-		}
-		catch(Exception e)
-		{
-			System.out.print("ServiceException encountered: ");
-		    System.out.println(e.getMessage());
-		    System.exit(-1);
-		}
-	}
+    public static void main(final String[] args) {
+        final Configuration config =
+                ServiceBusConfiguration.configureWithWrapAuthentication(
+                        "namespace",
+                        "user",
+                        "password",
+                        ".servicebus.windows.net",
+                        "-sb.accesscontrol.windows.net/WRAPv0.9");
+        final ServiceBusContract service = ServiceBusService.create(config);
+        try {
+            final ReceiveMessageOptions opts = ReceiveMessageOptions.DEFAULT;
+            opts.setReceiveMode(ReceiveMode.PEEK_LOCK);
+            while(true) {
+                final ReceiveSubscriptionMessageResult resultSubMsg = service.receiveSubscriptionMessage("topic",
+                        "subscription", opts);
+                final BrokeredMessage message = resultSubMsg.getValue();
+                if((message != null) && (message.getMessageId() != null)) {
+                    final Ticket t = JAXB.unmarshal(message.getBody(), Ticket.class);
+                    System.out.println(t.getTicketId());
+                    System.out.println(t.getCustomerId());
+                    System.out.println(t.getTicketDescription());
+                    System.out.println("-----------");
+                }
+                else {
+                    System.out.println("No new messages");
+                }
+            }
+        }
+        catch(final Exception e) {
+            System.out.print("ServiceException encountered: ");
+            System.out.println(e.getMessage());
+            System.exit(-1);
+        }
+    }
 }
-
